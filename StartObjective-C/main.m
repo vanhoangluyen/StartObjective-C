6224efff004b38c5e0c17a110cd3807d97abd5d9
LuyenMP <luyenmp@iMac-cua-Admin.local>
2018-06-25 14:35:51 +0700

    Initial Commit

---
+++ StartObjective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StartObjective-C
+//
+//  Created by LuyenMP on 6/25/18.
+//  Copyright © 2018 LuyenMP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
