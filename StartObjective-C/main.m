c5c3aa2183f1dc62824a9d181fe22c9aede8bf29
LuyenMP <vanhoangluyen@gmail.com>
2018-06-25 15:47:05 +0700

    add create variable, const

--- StartObjective-C/main.m
+++ StartObjective-C/main.m
@@ -11,7 +11,30 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //Print to console a line "Hello world!"
+        //        NSLog(@"Hello, World!");
+        //MARK: - create a variable
+        /*
+        int myInterger;
+        myInterger = 40;
+        NSLog(@"myVariable: %d",myInterger);
+        NSString *myString = @"Hello";
+        myString = @"Hello World";
+        NSLog(@"myString: %@",myString);
+        double myDouble = 40;
+        NSLog(@"myDouble: %f",myDouble);
+         */
+        //Mark: - create a const
+        /*
+        const int myConst = 40;
+        NSLog(@"my Const: %d",myConst);
+        NSString *const myString = @"my NSString";
+//        myString = @"change my String";
+        NSLog(@"my String: %@", myString);
+        */
+        //Mark: - Convert
+        int number = 123;
+        
     }
     return 0;
 }
